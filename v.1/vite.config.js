19c259b245768e9559f767dd31f0e5c315e29f1c
florareiro <ceciliaflora@outlook.com.br>
2024-04-10 10:22:47 -0300

    update vite config entry

--- v.1/vite.config.js
+++ v.1/vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
+        main: "./src/main.jsx",
       },
     },
   },
